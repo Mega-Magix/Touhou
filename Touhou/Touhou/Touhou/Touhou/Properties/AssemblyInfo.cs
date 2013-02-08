c27abffb2eecced15a600070e1187a966306ba3e
Matthew Robinson <omegachysis@outlook.com>
2013-02-08 17:29:18 -0500

    Fixed Visual Studio project
    Fixed all broken references to global libraries.  Moved all IronPython
    libraries to a local location.  Switched device unloading process to
    Windows (was Xbox, this was not going to work on a Windows computer
    obviously!).  Revamped project libraries.

--- Touhou/Touhou/Touhou/Touhou/Properties/AssemblyInfo.cs
+++ Touhou/Touhou/Touhou/Touhou/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // On Windows, the following GUID is for the ID of the typelib if this
 // project is exposed to COM. On other platforms, it unique identifies the
 // title storage container when deploying this assembly to the device.
-[assembly: Guid("f0cde42b-7f5b-456a-bbdc-06168888b80e")]
+[assembly: Guid("a96a82b9-0912-4bf1-b8ff-120e4a687d65")]
 
 // Version information for an assembly consists of the following four values:
 //
