36a702a4a4ebde4693b705eda779f1791716ea87
Matthew Robinson <omegachysis@outlook.com>
2013-02-08 18:18:31 -0500

    Modified template and create ExampleSprite.cs

--- Touhou/Touhou/ExampleSprite.cs
+++ Touhou/Touhou/ExampleSprite.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace Touhou.YourModuleName
+namespace Touhou.ExampleSprite
 {
     public class Touhou : Microsoft.Xna.Framework.Game
     {
--- Touhou/Touhou/Program.cs
+++ Touhou/Touhou/Program.cs
@@ -5,9 +5,6 @@ namespace Touhou
 #if WINDOWS || XBOX
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main(string[] args)
         {
             using (Touhou game = new Touhou())
@@ -18,4 +15,3 @@ namespace Touhou
     }
 #endif
 }
-
