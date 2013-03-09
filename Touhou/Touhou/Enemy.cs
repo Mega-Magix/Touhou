160000f26f98b81040c4470a284123ca37ca8858
Matthew Robinson <omegachysis@outlook.com>
2013-03-08 22:30:21 -0500

    Deleted old scripting system

--- Touhou/Touhou/Enemy.cs
+++ Touhou/Touhou/Enemy.cs
@@ -31,11 +31,6 @@ namespace Touhou.Battle
 
         public int health;
 
-        List<float> scriptWait = new List<float>();
-        List<float> scriptDelay = new List<float>();
-        List<int> scriptLoops = new List<int>();
-        List<string> scripts = new List<string>();
-
         Texture2D bulletTexture;
 
         public Enemy(Game game, Level level, int x, int radius, int speedX, int speedY, int health)
@@ -57,21 +52,6 @@ namespace Touhou.Battle
             velocity.Y = speedY;
 
             animation = new Effect.AnimatedTexture(animationTexture, position, 4, 0.2);
-        }
-
-        public void AddScript(string scriptName, float wait, float delay, int loops)
-        {
-            scriptWait.Add(wait);
-            scriptDelay.Add(delay);
-            scriptLoops.Add(loops);
-            //scripts.Add(scriptName);
-            //var python = Python.CreateEngine();
-            //ScriptScope scope = python.CreateScope();
-            //ScriptSource source = python.CreateScriptSourceFromFile("../../Scripts/Scripts/" + scriptName + ".py");
-            //scope.SetVariable("enemy", this);
-            //scope.SetVariable("level", level);
-            //scriptSources.Add(source);
-            //scriptScopes.Add(scope);
         }
 
         public Vector2 GetCenter()
@@ -108,16 +88,6 @@ namespace Touhou.Battle
             position += velocity * dt;
             animation.position = position;
             animation.Update(dt);
-
-            for (int i = 0; i < scripts.Count; i++)
-            {
-                scriptWait[i] -= dt;
-                if (scriptWait[i] <= 0.0f && scriptLoops[i] > 0)
-                {
-                    scriptLoops[i]--;
-                    scriptWait[i] = scriptDelay[i];
-                }
-            }
 
             if (position.Y >= level.screenHeight)
                 Destroy();
