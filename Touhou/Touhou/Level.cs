d009aece97b8759a9ecd131711f6fa7c86425954
Matthew Robinson <omegachysis@outlook.com>
2013-03-09 09:42:02 -0500

    Deleted testing commit

--- Touhou/Touhou/Level.cs
+++ Touhou/Touhou/Level.cs
@@ -9,8 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-// test change
-
 namespace Touhou.Battle
 {
     public class Level
